9924fd845e3d9a6b4e6be4908c9e87b5442a2438
Silvio Mori <silviomori@gmail.com>
2023-02-15 15:47:04 -0600

    Build and use suppliers

--- LambdasLab/src/main/java/com/technomori/javastudy/lambdaslab/BasicLambdas.java
+++ LambdasLab/src/main/java/com/technomori/javastudy/lambdaslab/BasicLambdas.java
@@ -1,14 +1,17 @@
 package com.technomori.javastudy.lambdaslab;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import com.technomori.javastudy.lambdaslab.interfaces.Printable;
+import com.technomori.javastudy.lambdaslab.interfaces.Retrievable;
 
 public class BasicLambdas {
 
 	public static void main(String[] args) {
 		BasicLambdas basicLambdas = new BasicLambdas();
 		basicLambdas.consumer();
+		basicLambdas.supplier();
 	}
 
 	private void consumer() {
@@ -22,4 +25,12 @@ public class BasicLambdas {
 		consumerPrintableMethodReference.accept("Consumer - Method Reference");
 	}
 
+	private void supplier() {
+		Retrievable<Integer> retrievable = () -> 77;
+		System.out.println("Retrievable: " + retrievable.retrieve());
+		
+		Supplier<Integer> supplier = () -> 77;
+		System.out.println("Supplier: "+ supplier.get());
+	}
 }
+
---
+++ LambdasLab/src/main/java/com/technomori/javastudy/lambdaslab/interfaces/Retrievable.java
@@ -0,0 +1,6 @@
+package com.technomori.javastudy.lambdaslab.interfaces;
+
+@FunctionalInterface
+public interface Retrievable<T> {
+	T retrieve();
+}
